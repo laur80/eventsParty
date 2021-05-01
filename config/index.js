2cfd0e172321e8fb3b32535eec15227851ff1235
laur80 <laur80raul@gmail.com>
2021-05-01 13:31:15 +0300

    inputsearch, addEvent, fix API_URL

--- config/index.js
+++ config/index.js
@@ -1,2 +1,3 @@
-export const API_URL = "http://localhost:1337";
-//   process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
+// "http://localhost:1337";
